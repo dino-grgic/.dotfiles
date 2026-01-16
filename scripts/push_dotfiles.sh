09133c29e2d416bd5fb2d0386f65eaea4341413d
Dino Grgic <dino.grgic1@gmail.com>
2026-01-16 16:38:49 +0100

    Updated dotfiles

---
+++ scripts/push_dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+cp -r ~/.config/nvim ~/.dotfiles/nvim
+cp -r ~/.config/i3 ~/.dotfiles/i3
+cp -r ~/.config/i3blocks ~/.dotfiles/i3blocks
+cp -r ~/.config/tmux ~/.dotfiles/tmux
+cp -r ~/.config/scripts ~/.dotfiles/scripts
+cp -r ~/.config/kittyy ~/.dotfiles/kitty
