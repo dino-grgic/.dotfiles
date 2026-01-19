0c065ecb844020572c9b6b7a60f08f6771679a12
Dino Grgic <dino.grgic1@gmail.com>
2026-01-19 17:49:39 +0100

    Updated dotfiles

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -2,9 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  -- Import molten.nvim for Jupyter integration
-  require 'custom.plugins.molten',
-  -- Import jupytext.nvim to edit .ipynb files
-  require 'custom.plugins.jupytext',
-}
+return {}
